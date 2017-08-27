1d466e91cfed5baa561194eb4af948a771f04a51
zettre <vaibhav.singhh1007@gmail.com>
2017-08-27 09:10:16 +0530

    session storing in mongodb

--- config/conf.js
+++ config/conf.js
@@ -1,5 +1,8 @@
 module.exports={
-	db:"mongodb://127.0.0.1:27017/chatter",
+	url:"mongodb://127.0.0.1:27017/chatter",
+	db:"chatter",
+	dbport:"27017",
+	dbip:"127.0.0.1",
 	port:3000,
 	secretKey:"avhshgs12712$%&"
 };
--- server.js
+++ server.js
@@ -9,12 +9,15 @@ var mongoose=require('mongoose');
 var bodyParser=require('body-parser');
 var session=require('express-session');
 var flash=require('express-flash');
+var MongoStore=require('connect-mongo')(session);
+
 
 var socket=require('./socket/sockets')(io);
 var secret=require('./config/conf');
 var User=require('./models/user');
 
-mongoose.connect(secret.db,function(error){
+
+mongoose.connect(secret.url,function(error){
 	if(error)
 	{
 		console.log(error);
@@ -25,6 +28,7 @@ mongoose.connect(secret.db,function(error){
 	}
 });
 
+
 app.use(express.static(__dirname+"/public"));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -33,6 +37,12 @@ app.use(session({
 	resave:true,
 	saveUninitialized:true,
 	secret:secret.secretKey,
+	store:new MongoStore({
+		db:secret.db,
+		host:secret.dbip,
+		port:secret.dbport,
+		url:secret.url
+	})
 }));
 app.use(flash());
 app.engine('ejs',engine);
@@ -43,8 +53,6 @@ var userRoutes=require('./routes/user');
 app.use(userRoutes);
 var chatRoutes=require('./routes/chat');
 app.use(chatRoutes);
-
-
 
 
 app.get('/',function(req,res,next){
@@ -61,6 +69,7 @@ app.get('/',function(req,res,next){
 app.get('*', function(req, res){
   res.status(404).send('<h1 align="center">Looks like you are in wrong place!</h1><hr><h1 align="center">Error 404</h1>');
 });
+
 
 server.listen(secret.port,function(){
 	console.log("Server Listening on Port 3000...");
